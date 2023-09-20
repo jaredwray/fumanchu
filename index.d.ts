e1927f5d340f115d20db3da3e03af3290bc4ec58
Jared Wray <me@jaredwray.com>
2023-09-20 10:14:14 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,3 @@
 // index.d.ts
-
-// Importing types from the handlebars package
-import { Handlebars } from 'handlebars';
-
-// Assuming helpers.js exports a function. Adjust as needed.
-interface HelpersFunction {
-    (config: { handlebars: typeof Handlebars }): void;
-}
-
-// Since you're exporting the handlebars instance itself:
-declare const handlebars: typeof Handlebars;
-
-export = handlebars;
-
+export * from 'handlebars';
 
