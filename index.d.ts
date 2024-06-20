528370f999dd0e21369cd25fcb8685588b8c8678
Jared Wray <me@jaredwray.com>
2024-06-20 13:27:25 -0600

    simplified it down

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,5 @@
-import { TemplateDelegate, RuntimeOptions, HelperDelegate, TemplateSpecification, compile, registerHelper } from 'handlebars';
-
-declare module 'handlebars' {
-    interface Handlebars {
-        create(): typeof Handlebars;
-    }
-}
-
-declare namespace Helpers {
-    type HelperFunction = (options?: { handlebars: typeof Handlebars }) => void;
-}
-
-declare const handlebars: {
-    compile: typeof compile;
-    registerHelper: typeof registerHelper;
-    TemplateDelegate: typeof TemplateDelegate;
-    RuntimeOptions: typeof RuntimeOptions;
-    HelperDelegate: typeof HelperDelegate;
-    TemplateSpecification: typeof TemplateSpecification;
-    create: typeof HandlebarsLib.create;
-}
-
-declare const helpers: Helpers.HelperFunction;
-
-declare const handlebarHelpers: Helpers.HelperFunction;
-
-export = handlebars;
+import HandlebarsLib from 'handlebars';
+declare const handlebars: typeof HandlebarsLib;
+declare let handlebarHelpers: any;
+export default handlebars;
 export { handlebarHelpers };
