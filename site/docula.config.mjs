7ea30c28a37ff927279e8ae398540775cb6af26a
Jared Wray <me@jaredwray.com>
2024-11-10 13:27:08 -0800

    upgrading cacheable to 1.8.4

--- site/docula.config.mjs
+++ site/docula.config.mjs
@@ -13,7 +13,7 @@ export const onPrepare = async config => {
 	const readmePath = path.join(process.cwd(), './README.md');
 	const readmeSitePath = path.join(config.sitePath, 'README.md');
 	const readme = await fs.promises.readFile(readmePath, 'utf8');
-	const updatedReadme = readme.replace('![Fumanchu](logo.png "Fumanchu")\n\n', '');
+	const updatedReadme = readme.replace('![Fumanchu](site/logo.svg "Fumanchu")\n\n', '');
 	console.log('writing updated readme to', readmeSitePath);
 	await fs.promises.writeFile(readmeSitePath, updatedReadme);
 };
