d326cf7139e1caa4a6be2ec6789bb998db8f4769
Jared Wray <me@jaredwray.com>
2024-12-15 12:20:09 -0800

    adding in date parsing for the date helper

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,11 +1,34 @@
+import {parseDate} from 'chrono-node';
+import dayjs from 'dayjs';
 import {type Helper} from '../helper-registry.js';
 
 const year = () => new Date().getFullYear().toString();
+
+const date = (humanReadableDate: string, formatString: string): string => {
+	const parsedDate = parseDate(humanReadableDate);
+	if (!parsedDate) {
+		throw new Error(`Unable to parse date: ${humanReadableDate}`);
+	}
+
+	// Format the parsed date using `dayjs`
+	return dayjs(parsedDate).format(formatString);
+};
 
 export const helpers: Helper[] = [
 	{
 		name: 'year',
 		category: 'date',
 		fn: year,
+	},
+	{
+		name: 'date',
+		category: 'date',
+		fn: date,
+	},
+	{
+		/* Adding this in for legacy support */
+		name: 'moment',
+		category: 'date',
+		fn: date,
 	},
 ];
--- test/helpers/date.test.ts
+++ test/helpers/date.test.ts
@@ -1,7 +1,9 @@
 import {describe, it, expect} from 'vitest';
+import dayjs from 'dayjs';
+import {parseDate} from 'chrono-node';
 import {helpers} from '../../src/helpers/date.js';
 
-describe('helpers', () => {
+describe('year', () => {
 	it('should have a "year" helper with the correct properties', () => {
 		// Find the 'year' helper in the array
 		const yearHelper = helpers.find(helper => helper.name === 'year');
@@ -23,5 +25,34 @@ describe('helpers', () => {
 		// Invoke the function
 		const currentYear = new Date().getFullYear().toString();
 		expect(yearHelper?.fn()).toBe(currentYear);
+	});
+});
+
+describe('date', () => {
+	it('should correctly calculate and format "5 years ago"', () => {
+		const dateHelper = helpers.find(helper => helper.name === 'date');
+		expect(dateHelper).toBeDefined();
+
+		const formattedDate = dateHelper?.fn('5 years ago', 'YYYY');
+		const expectedDate = dayjs().subtract(5, 'years').format('YYYY');
+		expect(formattedDate).toBe(expectedDate);
+	});
+
+	it('should correctly calculate and format "next Friday"', () => {
+		const dateHelper = helpers.find(helper => helper.name === 'date');
+		expect(dateHelper).toBeDefined();
+
+		const formattedDate = dateHelper?.fn('next Friday', 'MM/DD/YYYY');
+		const expectedDate = dayjs(parseDate('next Friday')).format('MM/DD/YYYY');
+		expect(formattedDate).toBe(expectedDate);
+	});
+
+	it('should throw an error for an invalid date format', () => {
+		const dateHelper = helpers.find(helper => helper.name === 'date');
+		expect(dateHelper).toBeDefined();
+
+		expect(() => dateHelper?.fn('invalid date', 'YYYY')).toThrowError(
+			'Unable to parse date: invalid date',
+		);
 	});
 });
