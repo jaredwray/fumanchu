a2a7891bc4d720491a29282831746120d59dbc33
Jared Wray <me@jaredwray.com>
2025-10-25 08:23:37 -0700

    chore: upgrading docula to 0.31.0

--- site/docula.config.mjs
+++ site/docula.config.mjs
@@ -1,9 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import process from 'node:process';
-import { DoculaHelpers } from 'docula';
-
-const helpers = new DoculaHelpers();
+import { Writr } from 'docula';
 
 export const options = {
 	githubPath: 'jaredwray/fumanchu',
@@ -18,13 +16,15 @@ export const onPrepare = async config => {
 	const readme = await fs.promises.readFile(readmePath, 'utf8');
 	let updatedReadme = readme.replace('![Fumanchu](site/logo.svg "Fumanchu")\n\n', '');
 
+	const writr = new Writr(updatedReadme);
+
 	let frontMatter = {};
 	frontMatter.title = 'Getting Started';
 	frontMatter.description = 'Fumanchu Getting Started Guide';
 	frontMatter.order = 1;
 
-	updatedReadme = helpers.setFrontMatterInContent(updatedReadme, frontMatter);
+	writr.frontMatter = frontMatter;
 
 	console.log('writing updated readme to', readmeSitePath);
-	await fs.promises.writeFile(readmeSitePath, updatedReadme);
+	await fs.promises.writeFile(readmeSitePath, writr.content);
 };
